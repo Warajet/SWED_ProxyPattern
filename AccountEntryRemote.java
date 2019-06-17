7f96101bbd4e7d92e9c662886693273c20ca1e42
warakornJet <wadh1n1@hotmail.com>
2019-06-17 18:10:50 +0200

    Delete AccountEntryRemote.java

--- AccountEntryRemote.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface AccountEntryRemote extends Remote {
-    public String getName() throws RemoteException;
-    public double getPrice() throws RemoteException;
-    public void setPrice(double price) throws RemoteException;
-}
