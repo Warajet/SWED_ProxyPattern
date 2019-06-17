0d08675507e22b2c9eaa001924607479680634cf
warakornJet <wadh1n1@hotmail.com>
2019-06-17 18:19:50 +0200

    Update AccountManagementServer.java

--- Server/AccountManagementServer.java
+++ Server/AccountManagementServer.java
@@ -3,6 +3,7 @@ import java.rmi.Naming;
 public class AccountManagementServer {
     public static void main(String[] args){
         try {
+            // Create Server Helper
             AccountManagementImpl accountServer = new AccountManagementImpl("Tim Cook");
             Naming.rebind("AccountManagementServer",accountServer);
             System.out.println("Server starts");
